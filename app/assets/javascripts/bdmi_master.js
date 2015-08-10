f216df2534d6dab95b5974a5bf0d1bbc8c7a0ff1
markhao <mark.q.hao@gmail.com>
2015-08-10 16:51:05 -0700

    Generate backbone

---
+++ app/assets/javascripts/bdmi_master.js
@@ -0,0 +1,13 @@
+window.BDMIMaster = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  BDMIMaster.initialize();
+});
