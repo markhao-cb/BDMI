dc436820b11233615c79e5c35409f7becca8d17d
markhao <mark.q.hao@gmail.com>
2015-08-10 20:34:35 -0700

    delete unnecessary file

--- db/migrate/20150811031535_addcolumntousers.rb
+++
@@ -1,8 +0,0 @@
-class Addcolumntousers < ActiveRecord::Migration
-  def change
-    add_column :users, :email, :string, null: false
-    add_column :users, :password_digest, :string, null: false
-    add_column :users, :session_token, :string, null: false
-    add_index :usrs, :email, :session_token, unique: true
-  end
-end
