b33364ec5b98217855148d9d9e4b9cc1e448785e
markhao <mark.q.hao@gmail.com>
2015-08-17 21:58:58 -0700

    fix load more button

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,9 +32,9 @@ movie = Movie.create(title:"Mission Impossible Rogue Nation", yr: 2015, score:7.
 image = movie.images.create(image_url:"http://redcarpetrefs.com/wp-content/uploads/2015/07/zz6.jpg")
 review = movie.reviews.create(title:"It's just Amazing!", body: "Tom Cruise!!!", author_id:1, grade:10)
 review2 = movie.reviews.create(title:"I don't like it.", body: "It's terrible, I left befor it's over.", author_id:2, grade:2)
-post = movie.posts.create(post_url:"https://photos-4.dropbox.com/t/2/AADv0z2kednIC-324QD-ddAuyriHyx4SEKg2LgIjUrHC_w/12/455194318/jpeg/32x32/1/_/1/2/11266619_10153122819937730_9045367438836263730_o.jpg/EKS99tYDGFwgBygH/2sTcy416CdPQdk4_oYwoHVTRZbEB389oyt_sq5HjTLs?size=1280x960&size_mode=2")
+post = movie.posts.create(post_url:"http://res.cloudinary.com/dypfv4yqq/image/upload/v1439859209/11266619_10153122819937730_9045367438836263730_o_iy9lck.jpg")
 #
 movie1 = Movie.create(title:"Ted 2", yr: 2015, score:6, votes: 333, director_id: 1)
 image1 = movie1.images.create(image_url:"http://geeknewsnetwork.net/wp-content/uploads/2015/06/2015_ted_2_movie-3840x2160.jpg")
 review1 = movie1.reviews.create(title:"This review is for Ted 2", body: "Laasdfnaoekqo qoenqvkeq oqerjn kjnqwerkj oqwernqvjqe jqwejqwvqe qweaafdqienqer qewasdkj.", author_id:1, grade:8)
-post1 = movie1.posts.create(post_url:"https://photos-6.dropbox.com/t/2/AACdKb2y9A2AMUFp9lHaLx1EOFDHNm1ukzJP_7uc5enBGQ/12/455194318/jpeg/32x32/1/_/1/2/Ted_poster.jpg/EKS99tYDGF4gBygH/KREkE6A0HE2w5ePMeV7vnXTkfJ94fh7tP6QLqUwmasM?size=1280x960&size_mode=2")
+post1 = movie1.posts.create(post_url:"http://res.cloudinary.com/dypfv4yqq/image/upload/v1439859213/Ted_poster_ydjjh5.jpg")
