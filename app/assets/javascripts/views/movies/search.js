6913034b957e2a0f8d08e8fba639d49fa030e55e
markhao <mark.q.hao@gmail.com>
2015-08-31 11:00:18 -0700

    Add listener for search, create dropdown view and templates

--- app/assets/javascripts/views/movies/search.js
+++ app/assets/javascripts/views/movies/search.js
@@ -10,6 +10,8 @@ BDMI.Views.Search = Backbone.CompositeView.extend({
   render: function() {
     var content = this.template();
     this.$el.html(content);
+    this.registerSearchListener();
+    this.attachSubviews();
     return this;
   },
 
@@ -25,6 +27,52 @@ BDMI.Views.Search = Backbone.CompositeView.extend({
         Backbone.history.navigate("search/person/"+formData[0], { trigger: true });
       }
     }
+  },
+
+  registerSearchListener: function() {
+    var typingTimer;                //timer identifier
+    var doneTypingInterval = 3000;  //time in ms, 3 seconds
+    var $input = this.$('#myInput');
+    //on keyup, start the countdown
+    $input.on('paste keypress', function() {
+      clearTimeout(typingTimer);
+      typingTimer = setTimeout(this.doneTyping, doneTypingInterval);
+    }.bind(this));
+  },
+
+  getSelectionText: function() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+  },
+
+  doneTyping: function() {
+    var formData = this.$('form').serilizeJSON().search;
+    if (formData[1] === "movie") {
+      this.movieSearchDrop(formData[0]);
+    } else {
+      Backbone.history.navigate("search/person/"+formData[0], { trigger: true });
+    }
+  },
+
+  movieSearchDrop: function(title) {
+    var searchedMovies = new BDMI.Collections.SearchedMovies();
+    var searchedMovie = searchedMovies.fetch({
+      data: {
+        title:title
+      },
+      processData: true,
+      success: function(collection) {
+      }.bind(this)
+    });
+    var dropDownView = new BDMI.Views.SearchDropDown({
+      collection: searchedMovies
+    });
+    this.addSubview('#dropdown',dropDownView);
   },
 
   flashAlert: function(messages) {
