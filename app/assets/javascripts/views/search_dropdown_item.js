ce15334925bbbfa4d6d77a5a1d953d1c981fc615
markhao <mark.q.hao@gmail.com>
2015-09-01 00:09:29 -0700

    Change template for movie dropdown

--- app/assets/javascripts/views/search_dropdown_item.js
+++ app/assets/javascripts/views/search_dropdown_item.js
@@ -1,5 +1,7 @@
 BDMI.Views.SearchDropDownItem = Backbone.View.extend({
-  template: JST['movie/search_dropdown_item'],
+  template_movie: JST['movie/search_dropdown_item'],
+
+  template_actor: JST['actors/search_dropdown_item'],
 
   className: 'search-dropdown-item',
 
@@ -13,19 +15,22 @@ BDMI.Views.SearchDropDownItem = Backbone.View.extend({
 
   render: function() {
     if (this.type === 'movie') {
-      var image_url = this.model.attributes.base_url + "original" +
+      this.poster_url = this.model.attributes.base_url + "original" +
       this.model.attributes.poster_path;
-      var title = this.model.attributes.title;
+      this.$el.html(this.template_movie({
+        poster_url: this.poster_url,
+        movie: this.model
+      }));
+      this.onRender();
     } else {
       var image_url = this.model.attributes.base_url + "original" +
       this.model.attributes.profile_path;
       var title = this.model.attributes.name;
+      this.$el.html(this.template_actor({
+        image_url: image_url,
+        title: title
+      }));
     }
-    var content = this.template({
-      image_url: image_url,
-      title: title
-    });
-    this.$el.html(content);
     return this;
   },
 
@@ -36,5 +41,18 @@ BDMI.Views.SearchDropDownItem = Backbone.View.extend({
     } else {
       Backbone.history.navigate('person/'+ this.model.id, { trigger: true });
     }
-  }
+  },
+
+  onRender: function() {
+    this.$('#star').raty('destroy');
+    var grade = this.model.attributes.vote_average / 2;
+    this.$('#star').raty({
+      starOff: "http://res.cloudinary.com/dypfv4yqq/image/upload/v1439888579/star-off_j7trzb.png",
+      starOn: "http://res.cloudinary.com/dypfv4yqq/image/upload/v1439888579/star-on_iezcg6.png",
+      starHalf: "http://res.cloudinary.com/dypfv4yqq/image/upload/v1439888579/star-half_w79ezb.png",
+      helf: true,
+      score: grade,
+      readOnly: true
+    });
+  },
 });
