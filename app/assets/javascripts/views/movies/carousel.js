a5b17ef080b03e7bbd673bc9711ab26f1bdfeea7
markhao <mark.q.hao@gmail.com>
2015-09-02 15:37:28 -0700

    Improve loading speed by specify the image size for intro

--- app/assets/javascripts/views/movies/carousel.js
+++ app/assets/javascripts/views/movies/carousel.js
@@ -12,8 +12,9 @@ BDMI.Views.CarouselItemView = Backbone.CompositeView.extend({
   },
 
   render: function() {
-    var image = this.model.images().first();
-    var content = this.template({ image: image });
+    var image_url = this.model.images().first().attributes.image_url.slice(0,48)+
+    "/c_lpad,w_800"+this.model.images().first().attributes.image_url.slice(48);
+    var content = this.template({ image_url: image_url });
     this.$el.html(content);
     return this;
   },
